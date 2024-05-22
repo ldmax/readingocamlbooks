88b1a19dcb14c12a67594fd712eee26ebb6c0b74
ldmax <lidanmax@163.com>
2024-05-23 00:36:32 +0800

    update

--- hello_world.ml
+++ hello_world.ml
@@ -1,3 +1,18 @@
 let v = "Hello, world!";;
 
 let () = Printf.printf "%s\n" v;;
+
+(* This is a function *)
+let cube x = x * x * x;;
+
+(* negative function *)
+let neg x = if x < 0 then true else false;;
+
+(* function adding to 10 *)
+let addtoten a b = a + b = 10;;
+
+let () = Printf.printf "%d\n" (cube 3);;
+
+let test x = if neg x then Printf.printf "Negative\n" else Printf.printf "Positive\n";;
+
+
